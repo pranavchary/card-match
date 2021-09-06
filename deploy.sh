6193a5b5a7324145801edd3b00f6c03548bb3214
Pranav Chary <pranavchary1993@gmail.com>
2021-09-05 19:02:28 -0500

    change main to master in deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pranavchary/card-match.git main:gh-pages
+git push -f git@github.com:pranavchary/card-match.git master:gh-pages
 
 cd -
